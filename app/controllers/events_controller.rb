2d2e869608d2123637e5dd27120b3e0123a7276c
jordles113 <jordan.t.romero@gmail.com>
2020-09-24 09:54:18 -0500

    wires up create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,5 +4,20 @@ class EventsController < ApplicationController
         events = Event.all 
         render json: EventSerializer.new(events).to_serialized_json 
     end 
+
+    def create 
+        event = Event.new(event_params)
+        event.save 
+        render json: EventSerializer.new(events).to_serialized_json
+    end 
+
+
+
+
+    private 
+
+    def event_params
+        params.permit(:name, :description, :location, :duration, :cost)
+    end 
 end
 
