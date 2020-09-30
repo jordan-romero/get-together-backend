838d4162e39fad97074329fd9157cb4f4ca9d4e3
jordles113 <jordan.t.romero@gmail.com>
2020-09-30 10:22:43 -0500

    adds create and update to occasions controller

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -4,11 +4,30 @@ class OccasionsController < ApplicationController
         occasions = Occasion.all 
         render json: OccasionSerializer.new(occasions).to_serialized_json
     end 
+
+    def create 
+        # byebug
+        occasion = Occasion.new(occasion_params)
+        if occasion.save 
+            render json: OccasionSerializer.new(occasion).to_serialized_json
+        else 
+            render json: {errors: occasion.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end 
+    end 
+
+    def update 
+        occasion = Occasion.find_by(id: params[:id])
+        if occasion.update(occasion_params)
+            render json: OccasionSerializer.new(occasion).to_serialized_json
+        else 
+            render json: {errors: occasion.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end 
+    end 
 end
 
 
 private 
 
 def occasion_params 
-    params.permit(name, date, time, event_name)
+    params.permit(:name, :date, :time, :event_name)
 end 
