ab38a04eface5f3637bb3b07ff3fdc76d7cedb15
jordles113 <jordan.t.romero@gmail.com>
2020-10-01 12:41:47 -0500

    adds nested route for event creation in occasions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :events, only: [:create, :update, :destroy, :index]
-  resources :occasions, only: [:create, :update, :destroy, :index]
+  resources :occasions, only: [:create, :update, :destroy, :index] do
+    resources :events, only: [:create]
+  end 
   resources :categories, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
